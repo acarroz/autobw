98bfbac3c5172ccc1d6c7cf219557be3fdc964b0
Alvaro Carroz <alvaro.carroz@gmail.com>
2011-11-21 13:02:15 -0430

    First version

---
+++ autobw.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+maxdownspeed="90.0"
+maxupspeed="20.0"
+
+regdownspeed="60.0"
+regupspeed="15.0"
+
+wincount=$(smbtree -S -N -b | grep '\\\\' | grep -v "`hostname`" | wc -l)
+currdownspeed=$(deluge-console "config max_download_speed" | sed 's/  max_download_speed: //g')
+
+if [ $wincount -gt 0 ]; then
+  if [ $currdownspeed != $regdownspeed ]; then
+    deluge-console "config --set max_download_speed $regdownspeed"
+    deluge-console "config --set max_upload_speed $regupspeed"
+  fi
+else
+  if [ $currdownspeed != $maxdownspeed ]; then
+    deluge-console "config --set max_download_speed $maxdownspeed"
+    deluge-console "config --set max_upload_speed $maxupspeed"
+  fi
+fi
