e250f262c21f31812ede00d0d79365d1c3656515
Alvaro Carroz <alvaro.carroz@gmail.com>
2011-11-25 20:02:51 -0430

    Added a basic log file. Fixed a typo in the README.

--- autobw.sh
+++ autobw.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+logfile=$(echo "$0.log" | sed "s/.sh//g" | sed "s/.\///g")
+
 maxdownspeed="90.0"
 maxupspeed="30.0"
 
@@ -8,15 +10,20 @@ regupspeed="15.0"
 
 wincount=$(smbtree -S -N -b | grep '\\\\' | grep -v "`hostname`" | wc -l)
 currdownspeed=$(deluge-console "config max_download_speed" | sed 's/  max_download_speed: //g')
+now=$(date +"%F %T")
 
 if [ $wincount -gt 0 ]; then
   if [ $currdownspeed != $regdownspeed ]; then
     deluge-console "config --set max_download_speed $regdownspeed"
     deluge-console "config --set max_upload_speed $regupspeed"
+
+    echo "$now	REG	$wincount hosts found" >> $logfile
   fi
 else
   if [ $currdownspeed != $maxdownspeed ]; then
     deluge-console "config --set max_download_speed $maxdownspeed"
     deluge-console "config --set max_upload_speed $maxupspeed"
+
+    echo "$now	MAX	$wincount hosts found" >> $logfile
   fi
 fi
